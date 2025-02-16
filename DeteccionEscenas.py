acf5a8162937854245fb0c6c209bad12a31e8e67
DavidPardo00 <pardodavid00@gmail.com>
2025-02-16 12:19:43 +0100

    Modificaciones para mejora de script
    - Modificación path para búsqueda video
    - Creación de directorios de salida
    - Agregar metodo de detecion adaptativa como segunda opción
    - Guardado de imágenes de cada escena
    - Silenciado de ffmpeg por consola

--- DeteccionEscenas.py
+++ DeteccionEscenas.py
@@ -1,16 +1,32 @@
 from scenedetect import SceneManager, VideoManager
-from scenedetect.detectors import ContentDetector
-import subprocess
+from scenedetect.detectors import ContentDetector, AdaptiveDetector
+import subprocess, os, shutil
+from scenedetect.frame_timecode import FrameTimecode
+from scenedetect.video_splitter import split_video_ffmpeg
+from scenedetect.scene_manager import save_images
 
 # Archivo de video a analizar
-video_path = "CircleOfLove_RudyMancuso.mp4"
+video_folder = "original_videos"
+video_name = "CircleOfLove_RudyMancuso.mp4"
+video_path = os.path.join(video_folder, video_name)
+
+# Directorios de salida
+output_dir_images = "images_scenes"
+output_dir_clips = "clips_video"
+
+# Eliminar el contenido de las carpetas antes de procesar
+for folder in [output_dir_images, output_dir_clips]:
+    if os.path.exists(folder):
+        shutil.rmtree(folder)  # Borra toda la carpeta
+    os.makedirs(folder)  # La vuelve a crear vacía
 
 # Cargar el video con VideoManager
 video_manager = VideoManager([video_path])
 scene_manager = SceneManager()
 
-# Agregar detector de contenido con el umbral por defecto (27) o ajustarlo
-scene_manager.add_detector(ContentDetector(threshold=30))  
+# Agregar detector de contenido (umbral por defecto = 27) o adaptativo
+scene_manager.add_detector(ContentDetector(threshold=27))
+# scene_manager.add_detector(AdaptiveDetector())  
 
 # Procesar el video
 video_manager.set_downscale_factor()  # Escala el video para mejorar rendimiento
@@ -26,20 +42,29 @@ for i, scene in enumerate(scene_list):
     start_timecode, end_timecode = scene
     print(f"Escena {i+1}: {start_timecode} - {end_timecode}")
 
-# # Dividir el video en escenas  
-# for i, scene in enumerate(scene_list):
-#     start_time, end_time = scene
-#     output_file = f"escena_{i+1}.mp4"
+# Guardar imágenes de cada escena
+save_images(scene_list, video_manager, num_images=1, 
+            image_extension="jpg", output_dir=output_dir_images,
+            image_name_template="Scene-$SCENE_NUMBER")
 
-#     command = [
-#         "ffmpeg", "-i", video_path,
-#         "-ss", str(start_time.get_seconds()), "-to", str(end_time.get_seconds()),
-#         "-c", "copy", output_file
-#     ]
+print("Imágenes de cada escena generadas con éxito.")
 
-#     subprocess.run(command)
+# Dividir el video en escenas 
 
-# print("Cortes de escenas generados con éxito.")
+for i, scene in enumerate(scene_list):
+    start_time, end_time = scene
+    output_file = os.path.join(output_dir_clips, f"Clip_{i+1}.mp4")
+
+    command = [
+        "ffmpeg", "-i", video_path,
+        "-ss", str(start_time.get_seconds()), "-to", str(end_time.get_seconds()),
+        "-c", "copy", output_file
+    ]
+
+    subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # ffmpeg silenciado con stdout y stderr a DEVNULL
+
+print("Clips de escenas generados con éxito.")
 
 # Liberar recursos
 video_manager.release()
