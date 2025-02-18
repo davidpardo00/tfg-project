977267005a0e0ec6e6f49da14e29a4043b4a1915
DavidPardo00 <pardodavid00@gmail.com>
2025-02-18 19:02:19 +0100

    Actualizaciones varias
    - Añadidos varios print para comentar proceso de detección de escenas
    - Cambios de comentarios
    - Añadida elección por true o false de método de detección de escenas

--- tools.py
+++ tools.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-# ! Eliminacion de carpetas
+# ! Eliminacion de contenido de carpetas
 # Directorios de salida
 output_dir_images = "images_scenes"
 output_dir_clips = "clips_video"
@@ -10,5 +10,5 @@ output_dir_csv = "csv_files"
 # Eliminar el contenido de las carpetas antes de procesar
 for folder in [output_dir_images, output_dir_clips, output_dir_csv]:
     if os.path.exists(folder):
-        shutil.rmtree(folder)  # Borra toda la carpeta
-    os.makedirs(folder)  # La vuelve a crear vacía
+        shutil.rmtree(folder)   # Borra toda la carpeta
+    os.makedirs(folder)         # La vuelve a crear vacía
